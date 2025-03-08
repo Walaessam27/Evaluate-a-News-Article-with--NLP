76055a79549e8ad6d55c42bf32bba949cd9d1431
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2025-03-08 21:02:18 +0200

    add important note

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -1,4 +1,9 @@
 // js files
+//i tried to do everything and everything run as I want 
+// but after some time he does not run as I want 
+//and also I have a problem on my laptop it was 
+// inoperative but today I try to do it, and also I had an accident so I was late
+//  I am sorry for that
 import { handleSubmit } from './js/formHandler'
 
 
